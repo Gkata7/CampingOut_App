02ece143dd19207b68b0e708bbaad49a69635eba
Garrett Katashima <gkatashima@gmail.com>
2018-03-16 18:13:19 -0700

    commit

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     authRoutes = require("./routes/index");
 
+mongoose.connect(process.env.databaseURL);
 // mongoose.connect("mongodb://localhost/CampingOut");
-mongoose.connect("mongodb://kobe:warriors@ds217349.mlab.com:17349/campingout");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
